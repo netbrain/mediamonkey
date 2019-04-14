c40fe8edfe38e11603056d47a14cf6969c2aeac2
Kim Eik <kim@heldig.org>
2019-04-14 23:01:26 +0200

    added logging statements

--- main.go
+++ main.go
@@ -26,13 +26,17 @@ func main() {
 
 	err := filepath.Walk(*src, func(path string, info os.FileInfo, err error) error {
 		if info == nil || info.IsDir(){
+			log.Printf("found directory %s",path)
 			return nil
 		}
+		log.Printf("found file %s",path)
 
 		date,err := dateAndTime(path)
 		if err != nil {
 			return err
 		}
+		log.Printf("guessed date %s on %s",date.Format("2006/01/02"),path)
+
 		return copy(path,fmt.Sprintf("%s%s",filepath.Join(*dst,date.Format("2006/01/02150405")),strings.ToLower(filepath.Ext(path))))
 	})
 	if err != nil {
@@ -41,36 +45,39 @@ func main() {
 }
 
 func copy(src,dst string) error{
-	log.Println(fmt.Sprintf("%s -> %s",src,dst))
 	info,err := os.Stat(src)
 	if err != nil{
 		return err
 	}
-
-	input, err := ioutil.ReadFile(src)
-	if err != nil {
-		return err
-	}
-
-	if err := os.MkdirAll(filepath.Dir(dst),0744); err != nil {
-		return err
-	}
-
 	dstInfo,err  := os.Stat(dst)
-
 	if err == nil {
 		if dstInfo.Size() != info.Size() {
+			log.Printf("%s already exists, but integrity check failed",dst)
 			if err := os.Remove(dst); err != nil {
 				return err
 			}
 			return copy(src,dst)
 		}
+		log.Printf("%s already exists, no copy required",dst)
+		return nil
 	}
 
 	if !os.IsNotExist(err){
 		return err
 	}
 
+	log.Printf("reading file %s",src)
+	input, err := ioutil.ReadFile(src)
+	if err != nil {
+		return err
+	}
+
+
+	if err := os.MkdirAll(filepath.Dir(dst),0744); err != nil {
+		return err
+	}
+
+	log.Printf("copying %s to %s",src,dst)
 	err = ioutil.WriteFile(dst, input, 0644)
 	if err != nil {
 		return err
@@ -84,6 +91,7 @@ func dateAndTime(path string) (time.Time,error) {
 	if err != nil {
 		return time.Time{},err
 	}
+	defer f.Close()
 
 	x, err := exif.Decode(f)
 	if err != nil {
